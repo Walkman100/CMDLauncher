4e1a9c9c7d7e183311d8dde16a0c0e005484ca38
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2014-12-11 02:31:55 +0200

    UI code

--- CMDLauncher.Designer.vb
+++ CMDLauncher.Designer.vb
@@ -22,17 +22,106 @@ Partial Class CMDLauncher
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.btnCancel = New System.Windows.Forms.Button()
+        Me.grpFlag = New System.Windows.Forms.GroupBox()
+        Me.optK = New System.Windows.Forms.RadioButton()
+        Me.optC = New System.Windows.Forms.RadioButton()
+        Me.lnkFlag = New System.Windows.Forms.LinkLabel()
+        Me.grpFlag.SuspendLayout()
         Me.SuspendLayout()
         '
-        'Form1
+        'btnSave
         '
+        Me.btnSave.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnSave.Location = New System.Drawing.Point(77, 64)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(75, 23)
+        Me.btnSave.TabIndex = 0
+        Me.btnSave.Text = "Save"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'btnCancel
+        '
+        Me.btnCancel.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnCancel.Location = New System.Drawing.Point(158, 64)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(75, 23)
+        Me.btnCancel.TabIndex = 1
+        Me.btnCancel.Text = "Cancel"
+        Me.btnCancel.UseVisualStyleBackColor = True
+        '
+        'grpFlag
+        '
+        Me.grpFlag.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.grpFlag.Controls.Add(Me.lnkFlag)
+        Me.grpFlag.Controls.Add(Me.optC)
+        Me.grpFlag.Controls.Add(Me.optK)
+        Me.grpFlag.Location = New System.Drawing.Point(12, 12)
+        Me.grpFlag.Name = "grpFlag"
+        Me.grpFlag.Size = New System.Drawing.Size(221, 46)
+        Me.grpFlag.TabIndex = 2
+        Me.grpFlag.TabStop = False
+        Me.grpFlag.Text = "CMD Flag"
+        '
+        'optK
+        '
+        Me.optK.AutoSize = True
+        Me.optK.Location = New System.Drawing.Point(6, 19)
+        Me.optK.Name = "optK"
+        Me.optK.Size = New System.Drawing.Size(36, 17)
+        Me.optK.TabIndex = 0
+        Me.optK.TabStop = True
+        Me.optK.Text = "/k"
+        Me.optK.UseVisualStyleBackColor = True
+        '
+        'optC
+        '
+        Me.optC.AutoSize = True
+        Me.optC.Location = New System.Drawing.Point(48, 19)
+        Me.optC.Name = "optC"
+        Me.optC.Size = New System.Drawing.Size(36, 17)
+        Me.optC.TabIndex = 1
+        Me.optC.TabStop = True
+        Me.optC.Text = "/c"
+        Me.optC.UseVisualStyleBackColor = True
+        '
+        'lnkFlag
+        '
+        Me.lnkFlag.AutoSize = True
+        Me.lnkFlag.Location = New System.Drawing.Point(90, 21)
+        Me.lnkFlag.Name = "lnkFlag"
+        Me.lnkFlag.Size = New System.Drawing.Size(29, 13)
+        Me.lnkFlag.TabIndex = 2
+        Me.lnkFlag.TabStop = True
+        Me.lnkFlag.Text = "Help"
+        '
+        'CMDLauncher
+        '
+        Me.AcceptButton = Me.btnSave
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(398, 134)
-        Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.CancelButton = Me.btnCancel
+        Me.ClientSize = New System.Drawing.Size(245, 99)
+        Me.Controls.Add(Me.grpFlag)
+        Me.Controls.Add(Me.btnCancel)
+        Me.Controls.Add(Me.btnSave)
+        Me.Name = "CMDLauncher"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "CMDLauncher Settings"
+        Me.grpFlag.ResumeLayout(False)
+        Me.grpFlag.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
+    Friend WithEvents btnSave As System.Windows.Forms.Button
+    Friend WithEvents btnCancel As System.Windows.Forms.Button
+    Friend WithEvents grpFlag As System.Windows.Forms.GroupBox
+    Friend WithEvents lnkFlag As System.Windows.Forms.LinkLabel
+    Friend WithEvents optC As System.Windows.Forms.RadioButton
+    Friend WithEvents optK As System.Windows.Forms.RadioButton
 
 End Class
--- CMDLauncher.vb
+++ CMDLauncher.vb
@@ -1,3 +1,22 @@
 ﻿Public Class CMDLauncher
+    Private Sub CMDLauncher_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    End Sub
+
+    Private Sub lnkFlag_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles lnkFlag.LinkClicked
+        MsgBox("/k: Leave CMD window open after running script; /c: Close CMD after running script.", MsgBoxStyle.Information, "CMD Flag Info")
+    End Sub
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        If optC.Checked = True Then
+            My.Settings.Flag = "/c"
+        End If
+        If optK.Checked = True Then
+            My.Settings.Flag = "/k"
+        End If
+    End Sub
+
+    Private Sub btnCancel_Click(sender As Object, e As EventArgs) Handles btnCancel.Click
+        Application.Exit()
+    End Sub
 End Class
