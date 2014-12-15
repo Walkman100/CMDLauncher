d5c366cbc26b54862d35e3d286b4fe90bdbf3765
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2014-12-15 23:57:38 +0200

    Fixed a bug in Compile.bat
    ren command fails if there is already a file with the same name

--- Compile.bat
+++ Compile.bat
@@ -3,6 +3,7 @@
 %ProgramFiles%\MSBuild\12.0\bin\msbuild.exe /property:Configuration=Release CMDLauncher.sln
 
 %ProgramFiles%\NSIS\makensis.exe "NSIS Installer for CMDLauncher.nsi"
+del bin\Release\CMDLauncher-Portable.exe
 ren bin\Release\CMDLauncher.exe CMDLauncher-Portable.exe
 
 explorer.exe "%~dp0bin\Release"
