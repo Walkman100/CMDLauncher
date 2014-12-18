add0363aab368edf7e3b6b9c457141fc359a1ef2
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2014-12-18 02:01:45 +0200

    [Compiler] Patch to allow people who still use spaces to compile

--- Compile.bat
+++ Compile.bat
@@ -1,8 +1,8 @@
 @echo off
 
-%ProgramFiles%\MSBuild\12.0\bin\msbuild.exe /property:Configuration=Release CMDLauncher.sln
+"%ProgramFiles%\MSBuild\12.0\bin\msbuild.exe" /property:Configuration=Release CMDLauncher.sln
 
-%ProgramFiles%\NSIS\makensis.exe "NSIS Installer for CMDLauncher.nsi"
+"%ProgramFiles%\NSIS\makensis.exe" "NSIS Installer for CMDLauncher.nsi"
 del bin\Release\CMDLauncher-Portable.exe
 ren bin\Release\CMDLauncher.exe CMDLauncher-Portable.exe
 
