0a9b345e0cd25e2bbea899193d790b524e39da58
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2015-01-17 16:35:12 +0200

    Use new WinCompile repo to run compile script

--- Compile.bat
+++ Compile.bat
@@ -1,9 +1,1 @@
-@echo off
-
-"%ProgramFiles%\MSBuild\12.0\bin\msbuild.exe" /property:Configuration=Release CMDLauncher.sln
-
-"%ProgramFiles%\NSIS\makensis.exe" "NSIS Installer for CMDLauncher.nsi"
-del bin\Release\CMDLauncher-Portable.exe
-ren bin\Release\CMDLauncher.exe CMDLauncher-Portable.exe
-
-explorer.exe "%~dp0bin\Release"
+..\WinCompile\compile CMDLauncher
